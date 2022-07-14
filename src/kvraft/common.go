0fda2fdcd9097b6484491f78d3c3625ab1ef23a1
yclchuxue <3136165538@qq.com>
2022-07-14 09:28:29 +0800

    update some other

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -13,21 +13,27 @@ type PutAppendArgs struct {
 	Key   string
 	Value string
 	Op    string // "Put" or "Append"
+	CIndex	int64
+	OIndex  int64
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
 }
 
 type PutAppendReply struct {
+	Index int
 	Err Err
 }
 
 type GetArgs struct {
 	Key string
+	CIndex int64
+	OIndex int64
 	// You'll have to add definitions here.
 }
 
 type GetReply struct {
+	Index int
 	Err   Err
 	Value string
 }
